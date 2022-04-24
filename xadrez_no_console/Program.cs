8d182035be7143db6e1d5f2e89d784631361ed30
Marlon Salles <marlonsalles@hotmail.com>
2022-04-24 17:17:49 -0300

    Melhorando o printScreen

--- xadrez_no_console/Program.cs
+++ xadrez_no_console/Program.cs
@@ -7,12 +7,12 @@ namespace xadrez_no_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.ToPosicao());
-            string a = Console.ReadLine();
-
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.PutPecas(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+            tab.PutPecas(new Rei(tab, Cor.Preto), new Posicao(0, 1));
+            Screen.PrinTab(tab);
+            string stop = Console.ReadLine();
         }
     }
 }
